91f0890c35a7db118e4b92f70d97c1ac494522c8
TranTrung99999 <TranTrung99999@users.noreply.github.com>
2019-11-24 17:35:48 +0700

    CategoryDAO

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,13 +1,8 @@
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import daos.CategoryDAO;
 import entities.Category;
-import utils.jdbcUtil;
 
 public class MainClass {
 
@@ -25,76 +20,78 @@ public class MainClass {
 				break;
 			else if (cmd == 1) {
 //				List<Category> cat2 ; // tao ra cat2 nhung dang null-> khi truy suat thi bi nullpointer
-				List<Category> cats = new ArrayList<Category>(); // tao ra cats nhung da co vung nho, empty list ->
-																	// khong bi nullpointer
+//				List<Category> cats = new ArrayList<Category>(); // tao ra cats nhung da co vung nho, empty list ->
+				// khong bi nullpointer
 //				Class.forName("com.mysql.cj.jdbc.Driver"); // 1. load driver
 //				Connection con = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/AutgEfreya",
 //						"AutgEfreya", "q6qkzXDL12"); // 2. create Connection
-				Connection con = jdbcUtil.getConnection();
-				Statement st = con.createStatement(); // 3. create statement
-				ResultSet rs = st.executeQuery("SELECT * FROM Category"); // execute statement
-				System.out.println("Your result: ");
-				while (rs.next()) {
-					int id = rs.getInt("ID");
-					String name = rs.getString("Name");
-					// System.out.println(id + "|" + name);
-					Category cat = new Category(id, name);
-					cats.add(cat);
-					
-				}
+//				Connection con = jdbcUtil.getConnection();
+//				Statement st = con.createStatement(); // 3. create statement
+//				ResultSet rs = st.executeQuery("SELECT * FROM Category"); // execute statement
+//				System.out.println("Your result: ");
+//				while (rs.next()) {
+//					int id = rs.getInt("ID");
+//					String name = rs.getString("Name");
+//					// System.out.println(id + "|" + name);
+//					Category cat = new Category(id, name);
+//					cats.add(cat);
+//					
+//				}
+				List<Category> cats = CategoryDAO.getAll();
 				for (Category cat : cats) {
 					System.out.println(cat.id + " | " + cat.name);
 				}
-				con.close();
+				// con.close();
 			} else if (cmd == 2) {
 				System.out.println("Please enter ID: ");
 				int id = scan.nextInt();
-				List<Category> cats = new ArrayList<Category>();
-				Connection con = jdbcUtil.getConnection();
+//				List<Category> cats = new ArrayList<Category>();
+//				Connection con = jdbcUtil.getConnection();
 //				Class.forName("com.mysql.cj.jdbc.Driver"); // 1. load driver
 //				Connection con = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/AutgEfreya",
 //						"AutgEfreya", "q6qkzXDL12"); // 2. create Connection
 				// Statement st = con.createStatement(); // 3. create statement
 				// ResultSet rs = st.executeQuery("SELECT * FROM Category WHERE ID= " + id);
 				// //execute statement
-				PreparedStatement pst = con.prepareStatement("SELECT * FROM Category WHERE ID=?"); // prepare
-																									// statement
-				pst.setInt(1, id);
-				ResultSet rs = pst.executeQuery();
-				System.out.println("Your result: ");
-				if (rs.next()) {
-					String name = rs.getString("Name");
-					//System.out.println(id + "|" + name);
-					Category cat = new Category(id, name);
-					cats.add(cat);
-				}
-				for (Category cat : cats) {
+//				PreparedStatement pst = con.prepareStatement("SELECT * FROM Category WHERE ID=?"); // prepare																					// statement
+//				pst.setInt(1, id);
+//				ResultSet rs = pst.executeQuery();
+//				System.out.println("Your result: ");
+//				if (rs.next()) {
+//					String name = rs.getString("Name");
+//					//System.out.println(id + "|" + name);
+//					Category cat = new Category(id, name);
+//					cats.add(cat);
+//				}
+				Category cat = new CategoryDAO().getDetails(id);
+				if (cat != null) {
 					System.out.println(cat.id + " | " + cat.name);
 				}
-				con.close();
+				// con.close();
 			} else if (cmd == 3) {
 				scan.nextLine(); // xu ly ky tu ENTER -> clear buffer /flush
 				System.out.println("Please ENTER keyword: ");
 				String keyword = scan.nextLine();
-				List<Category> cats = new ArrayList<Category>();
-				Connection con = jdbcUtil.getConnection();
+//				List<Category> cats = new ArrayList<Category>();
+//				Connection con = jdbcUtil.getConnection();
 //				Class.forName("com.mysql.cj.jdbc.Driver"); // 1. load driver
 //				Connection con = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/AutgEfreya",
 //						"AutgEfreya", "q6qkzXDL12"); // 2. create Connection
-				PreparedStatement pst = con.prepareStatement("SELECT * FROM Category WHERE NAME LIKE ?");
-				pst.setString(1, "%" + keyword + "%");
-				ResultSet rs = pst.executeQuery();
-				while (rs.next()) {
-					int id = rs.getInt("ID");
-					String name = rs.getString("Name");
-					//System.out.println(id + " | " + name);
-					Category cat = new Category(id, name);
-					cats.add(cat);
-				}
+//				PreparedStatement pst = con.prepareStatement("SELECT * FROM Category WHERE NAME LIKE ?");
+//				pst.setString(1, "%" + keyword + "%");
+//				ResultSet rs = pst.executeQuery();
+//				while (rs.next()) {
+//					int id = rs.getInt("ID");
+//					String name = rs.getString("Name");
+//					// System.out.println(id + " | " + name);
+//					Category cat = new Category(id, name);
+//					cats.add(cat);
+//				}
+				List<Category> cats = CategoryDAO.search(keyword);
 				for (Category cat : cats) {
 					System.out.println(cat.id + " | " + cat.name);
 				}
-				con.close();
+//				con.close();
 			}
 		}
 		scan.close();
---
+++ src/daos/CategoryDAO.java
@@ -0,0 +1,61 @@
+package daos;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Category;
+import utils.jdbcUtil;
+
+public class CategoryDAO {
+	public static List<Category> getAll() throws Exception {
+		List<Category> cats = new ArrayList<Category>();
+		Connection con = jdbcUtil.getConnection();
+		Statement st = con.createStatement(); // 3. create statement
+		ResultSet rs = st.executeQuery("SELECT * FROM Category"); // execute statement
+		System.out.println("Your result: ");
+		while (rs.next()) {
+			int id = rs.getInt("ID");
+			String name = rs.getString("Name");
+			Category cat = new Category(id, name);
+			cats.add(cat);
+		}
+		con.close();
+		return cats;
+	}
+
+	public static Category getDetails(int id) throws Exception {
+		Category cat = null;
+		Connection con = jdbcUtil.getConnection();
+		PreparedStatement pst = con.prepareStatement("SELECT * FROM Category WHERE ID=?"); // prepare // statement
+		pst.setInt(1, id);
+		ResultSet rs = pst.executeQuery();
+		System.out.println("Your result: ");
+		if (rs.next()) {
+			String name = rs.getString("Name");
+			cat = new Category(id, name);
+		}
+		con.close();
+		return cat;
+	}
+
+	public static List<Category> search(String keyword) throws Exception {
+		List<Category> cats = new ArrayList<Category>();
+		Connection con = jdbcUtil.getConnection();
+		PreparedStatement pst = con.prepareStatement("SELECT * FROM Category WHERE NAME LIKE ?");
+		pst.setString(1, "%" + keyword + "%");
+		ResultSet rs = pst.executeQuery();
+		while (rs.next()) {
+			int id = rs.getInt("ID");
+			String name = rs.getString("Name");
+			Category cat = new Category(id, name);
+			cats.add(cat);
+		}
+		con.close();
+		return cats;
+	}
+
+}
